0d48958bfff06c582155ff11e9384a88a902c1e7
Sarvesh <atsarveshyadav@gmail.com>
2025-03-10 22:46:26 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function updateDayAndTime() {
+
+    const currentDayOfWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+
+    const today = new Date();
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const dayOfWeek = daysOfWeek[today.getDay()]; // retrieve current day as an index from 'Date' to get current day from 'daysOfWeek'
+
+
+    currentDayOfWeek.textContent = dayOfWeek;
+    currentUTCTime.textContent = today.getTime(); // represent current UTC time in milliseconds
+}
+
+updateDayAndTime(); // call the function once to display current day and UTC time
+
+setInterval(updateDayAndTime, 1000); // timer to call the function every 1000 milliseconds (1 second)
